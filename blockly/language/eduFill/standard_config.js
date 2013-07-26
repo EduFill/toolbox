84e668aea1243708d90058f94d3a946f89ae3187
Marc Wollenweber <marc.wollenweber@smail.inf.h-brs.de>
2013-07-26 12:03:09 +0200

    Rename config1.js to standard_config.js (also within frame.html, frame_test.html and frame_changemode.html) Move content of _edufill_messages.js into standard_config.js and delete the former file. Delete unused entries within standard_config.js

--- blockly/language/eduFill/standard_config.js
+++ blockly/language/eduFill/standard_config.js
@@ -26,6 +26,7 @@
 
 goog.require('Blockly.Language');
 
+
 Blockly.CONFIG_PRE_DEF_POSITIONS_S1 = 'S1';
 Blockly.CONFIG_PRE_DEF_POSITIONS_S2 = 'S2';
 Blockly.CONFIG_PRE_DEF_POSITIONS_S3 = 'S3';
@@ -114,12 +115,19 @@ Blockly.CONFIG_IK_SOLVER =
     [[Blockly.CONFIG_IK_SOLVER_ANALYTICAL, 'ANALYTICAL'],
     [Blockly.CONFIG_IK_SOLVER_GEOMETRICAL, 'GEOMETRICAL']];
 
-/* 
- * "/global_name"
- * "relative_name"
- * '~private_name'
- * 'default_param'
-*/
+
+Blockly.CONFIG_REF_FRAME_BASE = 'base';
+Blockly.CONFIG_REF_FRAME_ARM = 'arm';
+Blockly.CONFIG_REF_FRAME_GRIPPER = 'finger';
+
+Blockly.CONFIG_REF_FRAME_PY_BASE = '\"/base_link\"';
+Blockly.CONFIG_REF_FRAME_PY_ARM = '\"/arm_link_0\"';
+Blockly.CONFIG_REF_FRAME_PY_GRIPPER = '\"/arm_link_5\"';
+
+Blockly.CONFIG_REF_FRAME = 
+    [[Blockly.CONFIG_REF_FRAME_BASE, 'BASE'],
+     [Blockly.CONFIG_REF_FRAME_ARM, 'ARM'],
+     [Blockly.CONFIG_REF_FRAME_GRIPPER, 'GRIPPER']];
 
 Blockly.CONFIG_PARAMETER_OPEN = '\'/script_server/gripper/open\'';
 Blockly.CONFIG_PARAMETER_CLOSE = '\'/script_server/gripper/close\'';
@@ -162,3 +170,109 @@ Blockly.CONFIG_PARAMETER_SERVER =
      //[Blockly.CONFIG_PARAMETER_PRIVATE, 'PRIVATE'],
      //[Blockly.CONFIG_PARAMETER_DEFAULT, 'DEFAULT']
     ];
+    
+
+//
+// The entries below do not need to be changed and partially shouldn't be modified at all!
+//
+
+Blockly.ChangeModeTooltip = 'ChangeMode (checkbox): Activate the Changemode whenever a deeper view into the blocks functioning is required.';
+
+// Block colour
+Blockly.LANG_LOW_LEVEL_COLOUR = 0;
+Blockly.LANG_ROS_MESSAGE_COLOUR = 30;
+Blockly.LANG_ROS_NODE_COLOUR = 30;
+Blockly.LANG_ACTION_COLOUR = 60;
+Blockly.LANG_MID_LEVEL_COLOUR = 90;
+Blockly.LANG_HIGH_LEVEL_COLOUR = 180;
+Blockly.LANG_UNITS_COLOUR = 190;
+Blockly.LANG_DUMMY_COLOUR = 330;
+
+// Low level Blocks.
+Blockly.LANG_LOW_LEVEL_ROS_INIT_NODE_EMPTY = ''
+Blockly.LANG_LOW_LEVEL_ROS_INIT_NODE_TRUE = 'true';
+Blockly.LANG_LOW_LEVEL_ROS_INIT_NODE_FALSE = 'false';
+
+Blockly.LANG_LOW_LEVEL_IMPORT_MSG = 'import';
+Blockly.LANG_LOW_LEVEL_FROM_IMPORT_MSG = 'from x import';
+Blockly.LANG_LOW_LEVEL_MAIN_FRAME = 'import frame';
+
+// Please do not modify! Directly related to ROS commands
+Blockly.LANG_LOW_LEVEL_ROS_LOG_EMPTY = '';
+Blockly.LANG_LOW_LEVEL_ROS_LOG_DEBUG = 'debug';
+Blockly.LANG_LOW_LEVEL_ROS_LOG_INFO = 'info';
+Blockly.LANG_LOW_LEVEL_ROS_LOG_WARN = 'warn';
+Blockly.LANG_LOW_LEVEL_ROS_LOG_ERROR = 'err';
+Blockly.LANG_LOW_LEVEL_ROS_LOG_FATAL = 'fatal';
+
+// Mid level Blocks.
+Blockly.LANG_MID_LEVEL_MOVE_BASE_TWIST_LINEAR_X = 'linear.x (m/s)';
+Blockly.LANG_MID_LEVEL_MOVE_BASE_TWIST_LINEAR_Y = 'linear.y (m/s)';
+Blockly.LANG_MID_LEVEL_MOVE_BASE_TWIST_LINEAR_Z = 'linear.z (m/s)';
+Blockly.LANG_MID_LEVEL_MOVE_BASE_TWIST_ANGULAR_X = 'angular.x (rad/s)';
+Blockly.LANG_MID_LEVEL_MOVE_BASE_TWIST_ANGULAR_Y = 'angular.y (rad/s)';
+Blockly.LANG_MID_LEVEL_MOVE_BASE_TWIST_ANGULAR_Z = 'angular.z (rad/s)';
+Blockly.LANG_MID_LEVEL_MOVE_BASE_TWIST_SLEEP_TIME = 'sleep time';
+Blockly.LANG_MID_LEVEL_MOVE_BASE_TWIST_TOOLTIP = 'Move robot with twist. \n' + 
+                                                    'Linear values in meter per seconds \n' +
+                                                    'Angular values in radians per second';
+
+Blockly.LANG_MID_LEVEL_FIND_CUBE_TOOLTIP_GENERAL = 'and return its position according to the camera. \n' +
+                                         'TODO and THINK about: Return all cube positions and its framename in relation to base, arm, gripper';
+Blockly.LANG_MID_LEVEL_FIND_CUBE_TOOLTIP_BLUE = 'Search for a blue cube';
+Blockly.LANG_MID_LEVEL_FIND_CUBE_TOOLTIP_RED = 'Search for a red cube';
+Blockly.LANG_MID_LEVEL_FIND_CUBE_TOOLTIP_GREEN = 'Search for a green cube';
+Blockly.LANG_MID_LEVEL_FIND_CUBE_TOOLTIP_YELLOW = 'Search for a yellow cube';
+
+Blockly.LANG_MID_LEVEL_FIND_CUBE_COLOUR_RED = 0;
+Blockly.LANG_MID_LEVEL_FIND_CUBE_COLOUR_YELLOW = 60;
+Blockly.LANG_MID_LEVEL_FIND_CUBE_COLOUR_GREEN = 120;
+Blockly.LANG_MID_LEVEL_FIND_CUBE_COLOUR_BLUE = 180;
+
+Blockly.LANG_MID_LEVEL_MOVE_BASE_OA_TITLE = 'move base (with obstacle avoidance)';
+Blockly.LANG_MID_LEVEL_MOVE_BASE_OA_TOOLTIP = 'Use the navigation stack to move the robot to the specified coordinate and orientation. \n' +
+                        'A true or false will be returned depending on success or fail';
+
+// High level Blocks.
+Blockly.LANG_HIGH_LEVEL_FROM_IMPORT_1_MSG = 'from';
+Blockly.LANG_HIGH_LEVEL_FROM_IMPORT_2_MSG = 'import';
+
+Blockly.LANG_HIGH_LEVEL_MOVE_ROBOT_TO_POSITION_PLATFORM1 = 'platform1';
+Blockly.LANG_HIGH_LEVEL_MOVE_ROBOT_TO_POSITION_ENTRANCE = 'arena entrance';
+Blockly.LANG_HIGH_LEVEL_MOVE_ROBOT_TO_POSITION_OTHERS   = 'others';
+
+Blockly.LANG_HIGH_LEVEL_PLACE_CUBE_ONTO = 'onto';
+Blockly.LANG_HIGH_LEVEL_PLACE_CUBE_LEFT = 'to the left of';
+Blockly.LANG_HIGH_LEVEL_PLACE_CUBE_RIGHT = 'to the right of';
+Blockly.LANG_HIGH_LEVEL_PLACE_CUBE_INFRONT = 'in front of';
+Blockly.LANG_HIGH_LEVEL_PLACE_CUBE_PLATFORM1 = 'platform1';
+Blockly.LANG_HIGH_LEVEL_PLACE_CUBE_ROBOT = 'robot plate';
+Blockly.LANG_HIGH_LEVEL_PLACE_CUBE_OTHERS = 'other objects';
+
+Blockly.LANG_HIGH_LEVEL_WALL_FOLLOWER_LEFT  = 'left';
+Blockly.LANG_HIGH_LEVEL_WALL_FOLLOWER_RIGHT = 'right';
+
+Blockly.MoveRobotBaseDirectionString = [
+                          ['forward', ['0.1', '0', '0', '0', '0', '0']],
+                          ['backward', ['-0.1', '0', '0', '0', '0', '0']],
+                          ['left', ['0', '0.1', '0', '0', '0', '0']],
+                          ['right', ['0', '-0.1', '0', '0', '0', '0']],
+                          ['rotate_clockwise', ['0', '0', '0', '0', '0', '-0.1']],
+                          ['rotate_anticlockwise', ['0', '0', '0', '0', '0', '0.1']]
+                        ];
+
+Blockly.LANG_HIGH_LEVEL_FIND_AND_GRASP_OBJECT_CUBE = 'cube';
+
+// Connection types
+Blockly.LANG_CONNECTION_TYPE_ROS_MESSAGE = 'rosMessage';
+Blockly.LANG_CONNECTION_TYPE_ROS_NODE = 'rosNode';
+Blockly.LANG_CONNECTION_TYPE_FROM_IMPORT = 'FromImport';
+Blockly.LANG_CONNECTION_TYPE_IMPORT = 'Import';
+Blockly.LANG_CONNECTION_TYPE_TRANSFORM = 'Transform';
+Blockly.LANG_CONNECTION_TYPE_ARM_JOINT_POSITION = 'ArmJointPosition';
+Blockly.LANG_CONNECTION_TYPE_BASE_POSE_MSG = 'BasePoseMsg';
+Blockly.LANG_CONNECTION_TYPE_SIMPLE_ACTION_CLIENT = 'SimpleActionClient';
+Blockly.LANG_CONNECTION_TYPE_QUATERNION = 'Quaternion';
+
+// Additional entries
+Blockly.EDUFILL_ROS_PKG_NAME = 'edufill_blockly';
